28a821ab16d9df5783f1f72b63abed25756258c2
Kalidas R <kalidas.raju@reddit.com>
2022-04-22 12:24:35 -0500

    Setup mock webserver for testing

--- app/src/androidTest/java/com/example/mocktestapp/tests/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/mocktestapp/tests/MainActivityTest.kt
@@ -1,5 +1,8 @@
 package com.example.mocktestapp.tests
 
+import android.support.test.espresso.IdlingRegistry
+import android.support.test.rule.ActivityTestRule
+import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
@@ -11,6 +14,7 @@ import com.example.mocktestapp.OkHttpProvider
 import com.example.mocktestapp.R
 import com.example.mocktestapp.rule.MockServerRule
 import com.example.mocktestapp.util.FileReader
+import com.jakewharton.espresso.OkHttp3IdlingResource
 import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
@@ -25,8 +29,10 @@ import java.util.concurrent.TimeUnit
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
-//  @get:Rule
-//  val http3IdlingResource = HttpIdlingResourceRule(OkHttpProvider.getOkHttpClient())
+
+
+  @get:Rule
+  val http3IdlingResource = HttpIdlingResourceRule(OkHttpProvider.getOkHttpClient())
 
   @get:Rule
   val mockServerRule = MockServerRule()
@@ -36,20 +42,45 @@ class MainActivityTest {
 //  @get:Rule
 //  val activityScenario: ActivityScenario<MainActivity> = ActivityScenario.launch(MainActivity::class.java)
 
+//  @get:Rule
+//  val activityRule = ActivityTestRule(MainActivity::class.java, true, false)
+
+  //private val mockWebServer = MockWebServer()
+
   @Before
   fun setup() {
+    //mockWebServer.start(8080)
+  }
+
+  @After
+  fun teardown() {
+//    mockWebServer.shutdown()
+//    IdlingRegistry.getInstance().register(
+//      OkHttp3IdlingResource.create(
+//        "okhttp",
+//        OkHttpProvider.getOkHttpClient()
+//      )
+//    )
   }
 
   @Test
   fun testSuccessfulResponse() {
+//    mockWebServer.dispatcher = object : Dispatcher() {
+//      override fun dispatch(request: RecordedRequest): MockResponse {
+//        return MockResponse()
+//          .setResponseCode(200)
+//          .setBody(FileReader.readStringFromFile("success_response.json"))
+//      }
+//    }
     //activityRule.launchActivity(null)
 
+    launchActivity<MainActivity>()
     onView(withId(R.id.progress_bar))
-        .check(matches(withEffectiveVisibility(Visibility.GONE)))
+      .check(matches(withEffectiveVisibility(Visibility.GONE)))
     onView(withId(R.id.character_recyclerview))
-        .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+      .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
     onView(withId(R.id.textview))
-        .check(matches(withEffectiveVisibility(Visibility.GONE)))
+      .check(matches(withEffectiveVisibility(Visibility.GONE)))
   }
 
 //  @Test
@@ -72,7 +103,4 @@ class MainActivityTest {
 //        .check(matches(withText(R.string.something_went_wrong)))
 //  }
 
-  @After
-  fun teardown() {
-  }
 }
